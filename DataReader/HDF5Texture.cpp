83c3cbe7591f7766e5ea3480f2af4963ea09e0a2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-03 21:02:33 -0500

    [core] Implement texture reading

--- DataReader/HDF5Texture.cpp
+++ DataReader/HDF5Texture.cpp
@@ -1,19 +1,44 @@
 #include "HDF5Texture.hpp"
+
+#include <highfive/boost.hpp>
+#include <highfive/highfive.hpp>
 namespace DataReader
 {
-// Note that as soon as we use textures,
-// we run at frame rate (e.g. 60hz) instead of audio buffer rate
-// (e.g. maybe 1000hz if you have a decent enough soundcard).
 void HDF5_TextureReader::operator()()
 {
-  //   // Do some magic
-  //   int k = 0;
-  //   for(unsigned char& c : bytes)
-  //   {
-  //     c += k++ * inputs.bamboozle.value;
-  //   }
-  //
-  //   // Call this when the texture changed
-  //   outputs.image.texture.update(bytes.data(), 480, 270);
+  if(!dataset->isValid())
+    return;
+
+  if(dataset_dims.size() != 4)
+    return;
+  if(dataset_dims[3] != 3)
+    return;
+
+  int n_frames = dataset_dims[0];
+  int height = dataset_dims[1];
+  int width = dataset_dims[2];
+
+  if(width != outputs.image.texture.width || height != outputs.image.texture.height)
+    bytes = outputs.image.texture.allocate(width, height);
+
+  int frame = std::clamp(this->inputs.percent * (n_frames - 1.), 0., n_frames - 1.);
+  thread_local boost::multi_array<double, 4> result;
+  dataset->select({(unsigned)frame, 0, 0, 0}, {1, (unsigned)height, (unsigned)width, 3})
+      .read(result);
+
+  int sp = 0;
+  const auto& img = result[0];
+  for(int y = 0; y < height; y++)
+  {
+    const auto& row = img[y];
+    for(int x = 0; x < width; x++)
+    {
+      const auto& col = row[x];
+      for(int c = 0; c < 3; c++)
+        bytes[sp++] = 255 * col[c];
+      bytes[sp++] = 255;
+    }
+  }
+  outputs.image.texture.update(bytes.data(), width, height);
 }
 }
